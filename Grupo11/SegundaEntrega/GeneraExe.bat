913c337859e409d8d246a56ddbaf2ea52ef696ec
Griselda Otalora <griis.otalor@gmail.com>
2020-11-09 09:33:05 -0300

    parte de arbol funciona

--- Grupo11/SegundaEntrega/GeneraExe.bat
+++ Grupo11/SegundaEntrega/GeneraExe.bat
@@ -5,7 +5,7 @@ pause
 c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Segunda.exe
 pause
 pause
-Segunda.exe prueba.txt
+Segunda.exe pruebaAux.txt
 pause
 dot -Tpng intermedia.dot -o intermedia.png
 del lex.yy.c
