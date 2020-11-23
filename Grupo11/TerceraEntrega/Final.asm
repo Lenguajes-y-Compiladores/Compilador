dade0660953bb009a70c82be9a13ba1d3c5d00e7
Griselda Otalora <griis.otalor@gmail.com>
2020-11-22 23:49:18 -0300

    Revert "Revert "Funciona asignacion simple""
    This reverts commit 04082085fcb269f02fbe9f63b151acf5760d8753.

--- Grupo11/TerceraEntrega/Final.asm
+++ Grupo11/TerceraEntrega/Final.asm
@@ -13,25 +13,8 @@ contador                        	DD
 promedio                        	DD	
 actual                          	DD	
 suma                            	DD	
-_Prueba_txt_LyC_Tema_1!         	DB	"Prueba.txt LyC Tema 1",'$', 22 dup (?)
-@STDOUT@STDOUT                  	DD	
-_Ingrese_entero_para_actual:_   	DB	"Ingrese entero para actual:",'$', 28 dup (?)
-@STDIN@STDIN                    	DD	
-_0                              	DD	0
 _02_5                           	DD	02.5
-_0xA2B0                         	DD	41648
-_92                             	DD	92
-_1                              	DD	1
-_0_342                          	DD	0.342
-_256                            	DD	256
-@CONT@CONT                      	DD	
-_0b10                           	DD	2
-_52                             	DD	52
-_4                              	DD	4
-_La_suma_es:_                   	DB	"La suma es:",'$', 12 dup (?)
-_actual_mayor_a_2_y_dist_a_0    	DB	"actual mayor a 2 y dist a ",'$', 27 dup (?)
-_0b111010                       	DD	58
-_no_es_mayor_que_2              	DB	"no es mayor que ",'$', 17 dup (?)
+_3                              	DD	3
 
 .CODE
 
@@ -98,6 +81,12 @@ MOV es,ax
 FINIT
 FFREE
 
+fld _02.5
+fild _3
+fadd
+fstp @aux1
+fld @aux1
+fstp suma
 
 liberar:
 	ffree
